c27106650d7ead235d27577ddc0f91b2951c6034
carolina899 <carolina89209@gmail.com>
2026-04-20 19:24:54 -0700

    update

---
+++ books/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=250)
+    
+    def __str__(self):
+        return self.title
+    
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    image = models.CharField(max_length=2000)
+    created_at =models.CharField(auto_now_add=True)
+    update_at = models.CharField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    category = models.ForeignKey(Category, related_name='book', on_delete=models.CASCADE)
