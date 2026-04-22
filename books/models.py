15788e63dd4fbb4cbe7ba992e063664508ef7581
carolina899 <carolina89209@gmail.com>
2026-04-21 19:43:50 -0700

    update

--- books/models.py
+++ books/models.py
@@ -8,10 +8,10 @@ class Category(models.Model):
         return self.title
     
 class Book(models.Model):
-    title = models.CharField(max_length=250)
+    
+    title = models.CharField(max_length=500)
     author = models.CharField(max_length=250)
-    image = models.ImageField(upload_to='books_images/', blank=True, null=True)
-    summary = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='upload/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(Category, related_name='books', on_delete=models.CASCADE)
